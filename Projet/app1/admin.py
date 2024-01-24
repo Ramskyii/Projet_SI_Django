f30d65f30a3291540d3ab2d624a58341714da26c
Walid chennit <115035425+walidisil@users.noreply.github.com>
2024-01-24 22:40:47 +0000

    toutes les tables sont la normalement

--- Projet/app1/admin.py
+++ Projet/app1/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Patient
+from .models import Medecin
+from .models import RendezVous
+from .models import Dossier
+from .models import Diagnostic
+from .models import Ordonnance
+from .models import Medicament
 
-# Register your models here.
+admin.site.register(Patient)
+admin.site.register(Medecin)
+admin.site.register(RendezVous)
+admin.site.register(Dossier)
+admin.site.register(Diagnostic)
+admin.site.register(Ordonnance)
+admin.site.register(Medicament)
+
--- Projet/app1/models.py
+++ Projet/app1/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Patient(models.Model) : 
+    Nom_P = models.CharField(max_length = 50)
+    Prenom_P = models.CharField(max_length = 50)
+    DateNaissance = models.DateField()
+    Telephone_P = models.CharField(max_length = 10, help_text = 'Entrez un numero de telephone valable svp')
+    Adresse_P = models.CharField(max_length = 50)
+
+class Medecin(models.Model) : 
+    Nom_M = models.CharField(max_length = 50)
+    Prenom_M = models.CharField(max_length = 50)
+    Telephone_M = models.CharField(max_length = 10, help_text = 'Entrez un numero de telephone valable svp')
+
+class RendezVous(models.Model) : 
+    Date_rdv = models.DateTimeField()
+
+class Dossier (models.Model) : 
+    Derniere_modif = models.DateTimeField(auto_now=True)
+
+class Diagnostic(models.Model) : 
+    Desc_diag = models.CharField(max_length = 1000)
+
+class Ordonnance(models.Model) : 
+    Designation = models.CharField(max_length = 1000)
+
+class Medicament(models.Model) : 
+    Nom_Medic = models.CharField(max_length = 50)
