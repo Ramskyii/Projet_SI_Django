4ed581bf98e732d353b84cb7c3d384d83b410bba
Ramskyii <rami.boumala@gmail.com>
2024-01-26 17:18:04 +0000

    UI

--- Projet/app1/urls.py
+++ Projet/app1/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import affiche_rdv
 from .views import page_authentification
+from .views import ajouter_patient
+from .views import liste_patients
 
 urlpatterns = [ 
     path('',page_authentification,name='page_authentification'),          
-    path('RendezVous/',affiche_rdv,name='RendezVous'),
-
+    path('ajouter_patient/',ajouter_patient,name='ajouter_patient'),
+    path('liste_patients/',liste_patients, name ='liste_patients'),
 ]
 
--- Projet/app1/views.py
+++ Projet/app1/views.py
@@ -1,13 +1,26 @@
-from django.shortcuts import render
-from .models import RendezVous
+from django.shortcuts import render, redirect
+from .forms import PatientForm
+from .models import Patient
 
 
-def affiche_rdv(request):
-    rendezvous = RendezVous.objects.all()
-    return render(request, 'index.html', {'rendezvous': rendezvous})
+def liste_patients(request):
+    patients = Patient.objects.all()
+    return render(request, 'liste_patients.html', {'patients': patients})
 
 def page_authentification(request):
     return render(request, 'page_authentification.html')
 
+
+
+def ajouter_patient(request):
+    if request.method == 'POST':
+        form = PatientForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('ajouter_patient')  
+    else:
+        form = PatientForm()
+
+    return render(request, 'ajouter_patient.html', {'form': form})
 
 
