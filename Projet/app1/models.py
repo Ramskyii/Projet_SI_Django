4952036a6db92b4e2d17b6b5856d66b792d40a07
Walid chennit <115035425+walidisil@users.noreply.github.com>
2024-01-26 14:34:22 +0000

    default

--- Projet/app1/models.py
+++ Projet/app1/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Patient(models.Model) : 
-    Num_P = models.AutoField(primary_key=True)
+    Num_P = models.AutoField(primary_key=True, default=1)
     Nom_P = models.CharField(max_length = 50)
     Prenom_P = models.CharField(max_length = 50)
     DateNaissance = models.DateField()
@@ -11,36 +11,36 @@ class Patient(models.Model) :
     num_dossier = models.OneToOneField('Dossier',on_delete = models.CASCADE)
 
 class Medecin(models.Model) : 
-    Num_M = models.AutoField(primary_key=True)
+    Num_M = models.AutoField(primary_key=True, default=1)
     Nom_M = models.CharField(max_length = 50)
     Prenom_M = models.CharField(max_length = 50)
     Telephone_M = models.CharField(max_length = 10, help_text = 'Entrez un numero de telephone valable svp')
     Num_rendezvous = models.ForeignKey('RendezVous', on_delete = models.CASCADE)
 
 class RendezVous(models.Model) : 
-    Num_rdv = models.AutoField(primary_key=True)
+    Num_rdv = models.AutoField(primary_key=True, default=1)
     Date_rdv = models.DateTimeField()
     Num_Diagnostic = models.OneToOneField('Diagnostic',on_delete = models.CASCADE) 
     
 
 class Dossier(models.Model) : 
-    Num_D = models.AutoField(primary_key=True)
+    Num_D = models.AutoField(primary_key=True, default=1)
     Derniere_modif = models.DateTimeField(auto_now=True)
     Num_Patient = models.OneToOneField(Patient,on_delete = models.CASCADE)
 
 class Diagnostic(models.Model) : 
-    Num_Diag = models.AutoField(primary_key=True)
+    Num_Diag = models.AutoField(primary_key=True, default=1)
     Desc_diag = models.CharField(max_length = 1000)
     Num_rendezvous = models.OneToOneField(RendezVous,on_delete = models.CASCADE)
     Num_ordonnance = models.OneToOneField('Ordonnance',on_delete = models.CASCADE)
 
 class Ordonnance(models.Model) : 
-    Num_ord = models.AutoField(primary_key=True)
+    Num_ord = models.AutoField(primary_key=True, default= 1)
     Designation = models.CharField(max_length = 1000)
     Num_diagnostic = models.OneToOneField(Diagnostic,on_delete = models.CASCADE)
     Medicaments = models.ManyToManyField('Medicament')
     
 
 class Medicament(models.Model) : 
-    Code_Medic = models.AutoField(primary_key=True)
+    Code_Medic = models.AutoField(primary_key=True, default=1)
     Nom_Medic = models.CharField(max_length = 50)
